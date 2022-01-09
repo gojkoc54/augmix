67670fd3a5fd38a84c09a99834cc3a72ae07b8e0
Gojko Cutura <gojkoc54@gmail.com>
2022-01-09 12:19:46 +0100

    execution logs for standard case for alexnet

--- src/scripts/run_params_ablation.sh
+++ src/scripts/run_params_ablation.sh
@@ -11,29 +11,30 @@ printf "\n\nExecuting without augmix \n\n"
 echo "NO augmix" >> params_ablation_log.txt
 python3 main.py --should-augmix 0 --mixture-width 0 --mixture-depth 0 \
     --epochs ${epochs} --num-workers ${num_workers}
-    
+
 # Executing AugMix with default parameters 
 printf "\n\nExecuting augmix with default parameters (WIDTH = 3, DEPTH = 4)\n\n"
 echo "width = 3; depth = 4" >> params_ablation_log.txt
 python3 main.py --epochs ${epochs} --num-workers ${num_workers}
 echo "done" >> params_ablation_log.txt
 
-# Varying the width while keeping the depth default (4)
-for width in "${widths[@]}"
-do
-    printf "\n\nExecuting for parameters: WIDTH = ${width} DEPTH = 4 \n\n"
-    echo "width = ${width}; depth = 4" >> params_ablation_log.txt
-    python3 main.py --mixture-width ${width} \
-        --epochs ${epochs} --num-workers ${num_workers}
-    echo "done" >> params_ablation_log.txt
-done
+# # Varying the width while keeping the depth default (4)
+# for width in "${widths[@]}"
+# do
+#     printf "\n\nExecuting for parameters: WIDTH = ${width} DEPTH = 4 \n\n"
+#     echo "width = ${width}; depth = 4" >> params_ablation_log.txt
+#     python3 main.py --mixture-width ${width} \
+#         --epochs ${epochs} --num-workers ${num_workers}
+#     echo "done" >> params_ablation_log.txt
+# done
 
-# Varying the depth while keeping the width default (3)
-for depth in "${depths[@]}"
-do
-    printf "\n\nExecuting for parameters: WIDTH = 3 DEPTH = ${depth} \n\n"
-    echo "width = 3; depth = ${depth}" >> params_ablation_log.txt
-    python3 main.py --mixture-depth ${depth} \
-        --epochs ${epochs} --num-workers ${num_workers}
-    echo "done" >> params_ablation_log.txt
-done
+# # Varying the depth while keeping the width default (3)
+# for depth in "${depths[@]}"
+# do
+#     printf "\n\nExecuting for parameters: WIDTH = 3 DEPTH = ${depth} \n\n"
+#     echo "width = 3; depth = ${depth}" >> params_ablation_log.txt
+#     python3 main.py --mixture-depth ${depth} \
+#         --epochs ${epochs} --num-workers ${num_workers}
+#     echo "done" >> params_ablation_log.txt
+# done
+
